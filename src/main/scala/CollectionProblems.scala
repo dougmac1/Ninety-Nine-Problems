2476733ac7010fd8f2e37e8376f9a6913c4e93c7
Douglas McCallum <douglas.mccallum@digital.hmrc.gov.uk>
2018-04-19 11:52:34 +0100

    problem 7 complete, tests pass

--- src/main/scala/CollectionProblems.scala
+++ src/main/scala/CollectionProblems.scala
@@ -1,29 +1,34 @@
 class CollectionProblems {
 
-  def lastElement(list : List[Int]) : Int = {
+  def lastElement(list: List[Int]): Int = {
     list.last
   }
 
-  def penultimate(list : List[Int]) : Int = {
+  def penultimate(list: List[Int]): Int = {
     list.init.last
   }
 
-  def nthNumber(number : Int, list : List[Int]) : Any = {
+  def nthNumber(number: Int, list: List[Int]): Any = {
     if (number >= 0) list(number)
   }
 
-  def listSize(list : List[Int]) : Int = {
+  def listSize(list: List[Int]): Int = {
     list.length
   }
 
-  def reversed(list : List[Int]) : List[Int] = {
+  def reversed(list: List[Int]): List[Int] = {
     list.reverse
   }
 
-  def palindrome(list : List[Int]) : Boolean = {
+  def palindrome(list: List[Int]): Boolean = {
     list == list.reverse
   }
 
+  def flatten(list: List[Any]): List[Any] = list flatMap {
+    case i: List[_] => flatten(i)
+    case e => List(e)
+  }
 
 
 }
+
