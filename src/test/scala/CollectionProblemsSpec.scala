02cc021fdeaf25f8036d06ecac273ea9168eb652
Douglas McCallum <douglas.mccallum@digital.hmrc.gov.uk>
2018-04-19 10:48:24 +0100

    refactoring tests so they read correctly

--- src/test/scala/CollectionProblemsSpec.scala
+++ src/test/scala/CollectionProblemsSpec.scala
@@ -6,22 +6,24 @@ class CollectionProblemsSpec extends WordSpec with MustMatchers {
 
     val TDD = new CollectionProblems
 
-    "when given a list of 1,1,2,3,5,8 return the last element in the list '8'" in {
+    "return the last element in the list '8' when given a list of 1,1,2,3,5,8 " in {
 
       TDD.lastElement(List(1,1,2,3,5,8)) mustEqual 8
     }
 
-    "when given a list off 1,1,2,3,5,8 return the penultimate number in the list '5'" in {
+    "return the penultimate number in the list '5' when given a list off 1,1,2,3,5,8 " in {
       TDD.penultimate(List(1,1,2,3,5,8)) mustEqual 5
     }
 
-    "when given a list of 1,1,2,3,5,8 and the nth of '2' return the nth number" in {
+    "return the nth number when given a list of 1,1,2,3,5,8 and the nth of '2' " in {
       TDD.nthNumber(2, List(1,1,2,3,5,8)) mustEqual 2
     }
 
-    "when given a list of 1,1,2,3,5,8 return the number of elements in the list" in {
+    "return the number of elements in the list when given a list of 1,1,2,3,5,8 " in {
       TDD.listSize(List(1,1,2,3,5,8)) mustEqual 6
     }
+
+    
   }
 
 }
